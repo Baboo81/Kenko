a3b5b9e39767180f5866bf3c4b5c58b2eccc7a77
Baboo81 <magicbaboo@gmail.com>
2024-12-19 13:50:39 +0100

    PHP les tableaux

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Library CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link href="./style/reset.css" rel="stylesheet">
-        <link href="./style/style.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        <title>Kenko</title>
-    </head>
-    <body>
-        
-        <h1>Affichage des recettes</h1>
-
-      <?php
+<?php
         $recettes = [
             [
                 'titre' => 'Cassoulet <br />',
@@ -43,10 +27,42 @@
             ],
         ];
 
-        if (array_key_exists('titre', $recettes)) {
-            echo 'La clé "titre" se trouve dans le tableau !';
-        };
       ?>
-        
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Library CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link href="./assets/style/reset.css" rel="stylesheet">
+        <link href="./assets/style/style.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <title>Kenko</title>
+    </head>
+    <body>
+        <main>
+            <div class="conatiner">
+                <div class="row">
+                    <div class="col-md-12">
+                    <h1 class="caveatTitle text-center my-5">Affichage des recettes</h1>
+
+                    <!-- Boucle sur les recettes -->
+                     <?php foreach ($recettes as $recette) :  ?>
+                     <!--  Si la clé existe et a pour valeur "Faux", on affiche -->
+                     <?php if (array_key_exists ('estActive', $recette) && $recette['estActive'] == false) : ?>
+
+                                <article>
+                                    <h3 class="m-3"><?php echo $recette['titre']; ?></h3>
+                                    <p class="m-3"><?php echo $recette['recette']; ?></p>
+                                    <i class="m-3"><?php echo $recette['auteur']; ?></i>
+                                </article>
+                            <?php endif; ?>
+                        <?php endforeach ?>
+                    </div>
+                </div>
+            </div>
+        </main>
     </body>
 </html>
