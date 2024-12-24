b69a45e9054bf40014d039d0b498a30c490eeb9f
Baboo81 <magicbaboo@gmail.com>
2024-12-24 11:32:06 +0100

    HomePage

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
-
-<?php
-        $recettes = [
-            [
-                'titre' => 'Cassoulet <br />',
-                'recette' => 'Étape 1 : Mettre les flageolets à tremper toute une nuit, <br />Étape 2 : Mettre les flageolets avec les tomates ... <br />',
-                'auteur' => 'chrisrodriguez@hotmail.be',
-                'estActive' => true,
-            ],
-            [
-                'titre' => 'Couscous <br />',
-                'recette' => 'Étape 1 : Mettre les légumes dans la casserole, <br />Étape 2 : Mettre la viandes ... <br />',
-                'auteur' => 'ginettemanfroid@hotmail.be',
-                'estActive' => false,
-            ],
-            [
-                'titre' => 'Cassoulet <br />',
-                'recette' => 'Étape 1 : Mettre les flageolets à tremper toute une nuit, <br />Étape 2 : Mettre les flageolets avec les tomates ... <br />',
-                'auteur' => 'chrisrodriguez@hotmail.be',
-                'estActive' => true,
-            ],
-            [
-                'titre' => 'Escalope Milanaise <br />',
-                'recette' => 'Étape 1 : Paner les tranches de viande, <br />Étape 2 : Mettre la viandes ... <br />',
-                'auteur' => 'jeandujardin@hotmail.be',
-                'estActive' => false,
-            ],
-        ];
-
-      ?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -43,75 +12,12 @@
     </head>
     <body>
         <main>
-            <div class="conatiner">
+            <div class="container">
                 <div class="row">
                     <div class="col-md-12 text-center">
                     <h1 class="caveatTitle text-center my-5">Affichage des recettes</h1>
 
-                    <!-- Boucle sur les recettes -->
-                     <?php foreach ($recettes as $recette) :  ?>
-                     <!--  Si la clé existe et a pour valeur "Faux", on affiche -->
-                     <?php if (array_key_exists ('estActive', $recette) && $recette['estActive'] == false) : ?>
-
-                                <article>
-                                    <h3 class="m-3"><?php echo $recette['titre']; ?></h3>
-                                    <p class="m-3"><?php echo $recette['recette']; ?></p>
-                                    <i class="m-3"><?php echo $recette['auteur']; ?></i>
-                                </article>
-                            <?php endif; ?>
-                        <?php endforeach ?>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6 text-center my-5">
-                        <h3 class="my-5">Les fonctions</h3>
-                     
-                        <?php
-                            $recette = 'Etape 1 : des flageolets ! Etape 2 : de la saucisse';
-                            $longueur = strlen($recette);
-
-                            echo 'La phrase contient ' . $longueur .'caractères : ' . PHP_EOL . $recette;
-                            echo "<br />";
-                        ?>
-                        <?php
-                            echo str_replace('c', 'C', 'Le cassoulet c\'est très bon !');
-                            echo "<br />";
-                            $jour = date('d');
-                            $mois = date('m');
-                            $annee = date('Y');
-                            //Récup de l'heure :
-                            $heure = date('H');
-                            $minutes = date('i');
-
-                            echo 'Nous sommes le :' . $jour . '/' . $mois . '/' . $annee . " " . 'Et il est : ' . $heure . ':' . $minutes . PHP_EOL;
-                            echo "<br />";
-
-                            function recetteOK (array $recette) : bool {
-                                if (array_key_exists('estActive', $recette)) {
-                                    $result = $recette['estActive'];
-                                } else {
-                                    $result = false;
-                                }
-
-                                return $result;
-                            };
-
-                            $recetteCouscous = [
-                                        'titre' => 'Couscous <br />',
-                                        'recette' => 'Étape 1 : Mettre les légumes dans la casserole, <br />Étape 2 : Mettre la viandes ... <br />',
-                                        'auteur' => 'ginettemanfroid@hotmail.be',
-                                        'estActive' => false,
-                            ];
-                            $recetteCassoulet = [
-                                        'titre' => 'Cassoulet <br />',
-                                        'recette' => 'Étape 1 : Mettre les flageolets à tremper toute une nuit, <br />Étape 2 : Mettre les flageolets avec les tomates ... <br />',
-                                        'auteur' => 'chrisrodriguez@hotmail.be',
-                                        'estActive' => true,
-                            ];
-
-                            $recetteCouscousOK = recetteOK($recetteCassoulet);
-                        ?>
-                    </div>
+             
                 </div>
             </div>
         </main>
