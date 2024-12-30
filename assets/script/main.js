f8d130e3695feeea7e8d46015f8e389d4fed3986
Baboo81 <magicbaboo@gmail.com>
2024-12-30 15:25:08 +0100

    CSS

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+//btn animation :
+
+let btn = document.querySelector(".button");
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    btn.classList.add("animate");
+
+    setTimeout(() => {
+        btn.classList.remove("animate");
+    }, 600) //1s = 1000ms
+});
