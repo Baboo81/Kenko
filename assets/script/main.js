09d29e3bd33c4b623dd4fe4f869aab21d421df2a
Baboo81 <magicbaboo@gmail.com>
2025-01-02 09:55:25 +0100

    main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -32,6 +32,15 @@ btn.addEventListener("click", (e) => {
 
     setTimeout(() => {
         btn.classList.remove("animate");
-        console.log("Je suis ici :)))");
     }, 600) //1s = 1000ms
 }); 
+
+//Footer; map :
+
+    //Create map:
+const centroid = [50.71036, 4.36889];
+const map = L.map('mapid').setView(centroid, 16.4);
+
+    //Add tiles & marker:
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.marker([50.71036, 4.36889]).addTo(map);
