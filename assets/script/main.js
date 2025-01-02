55ba6e8e137ee5ade8853131685d40d7740df7d1
Baboo81 <magicbaboo@gmail.com>
2025-01-02 09:17:26 +0100

    main.js; script navBar

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,5 +1,27 @@
 "use strict";
 
+//navBar; color's switch:
+
+document.addEventListener('DOMContentLoaded', function() {
+    let navBar = document.querySelector('nav');
+
+    //Pour obtenir la page actuelle:
+    let currentPage = window.location.pathname;
+
+    //Définition de la couleur de typo en fct de la page :
+    switch (currentPage) {
+        case 'index.html':
+            navBar.style.color = '#282828';
+            break;
+        case 'kenko-ho.html':
+            navBar.style.color = '#f1efe9';
+            break;
+        case 'kenko-web.html':
+            navBar.style.color = '#f1efe9';
+            break;
+    }
+});
+
 //btn animation :
 
 let btn = document.querySelector(".button");
@@ -10,5 +32,6 @@ btn.addEventListener("click", (e) => {
 
     setTimeout(() => {
         btn.classList.remove("animate");
+        console.log("Je suis ici :)))");
     }, 600) //1s = 1000ms
 }); 
