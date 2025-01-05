0eb18053631757431de94e5fb3154b6571d574c9
Baboo81 <magicbaboo@gmail.com>
2025-01-05 14:51:42 +0100

    Added picto atouts

--- assets/script/main.js
+++ assets/script/main.js
@@ -58,3 +58,5 @@ const map = L.map('mapid').setView(centroid, 16.4);
     //Add tiles & marker:
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 L.marker([50.71036, 4.36889]).addTo(map);
+
+//Pop-up :
