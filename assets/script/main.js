1210bbd8b34a08d23fefd8028268691648e29ded
Baboo81 <magicbaboo@gmail.com>
2025-01-08 14:03:10 +0100

    cleaning

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,29 +1,5 @@
 "use strict";
 
-//navBar; color's switch:
-
-//document.addEventListener('DOMContentLoaded', function() {
-  //  let navBar = document.querySelector('nav');
-
-    //Pour obtenir la page actuelle:
-    //let currentPage = window.location.pathname;
-
-    //Définition de la couleur de typo en fct de la page :
-    //switch (currentPage) {
-      //  case './index.html':
-        //    navBar.style.color = '#282828';
-          //  break;
-        //case '../assets/src/views/kenko-ho.html':
-          //  navBar.style.color = '#f1efe9';
-            //break;
-        //case '../assets/src/views/kenko-web.html':
-          //  navBar.style.color = '#f1efe9';
-           // break;
-            //default:
-              //  navBar.style.color = '#f1efe9';
-   // }
-//});
-////////////////////////////// END /////////////////////////
 
 //logo animation :
 let logo = document.getElementById('logoAnimation');
@@ -38,19 +14,6 @@ setInterval(() => {
 }, 3000);
 ////////////////////////////// END /////////////////////////
 
-//btn animation :
-
-let btn = document.querySelector(".button");
-
-btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    btn.classList.add("animate");
-
-    setTimeout(() => {
-        btn.classList.remove("animate");
-    }, 600) //1s = 1000ms
-}); 
-////////////////////////////// END /////////////////////////
 
 //Footer; map :
 
